caf82f7c6945b913e005585f60a040bea2574c58
Lasse Herzog <herzogl.tin21@student.dhbw-heidenheim.de>
2023-07-21 13:52:09 +0200

    Extended docs for PeerClient.

--- client/src/peerClient.ts
+++ client/src/peerClient.ts
@@ -1,14 +1,18 @@
 import { DataConnection, Peer, PeerConnectOption } from 'peerjs'
 
+/**
+ * Wraps the PeerJS {@link Peer} class for asynchronous use.
+ * TODO Define timeout behavior
+ */
 class PeerClient extends Peer {
     private constructor() {
         super()
-
-        this.on('connection',
-            dataConnection => dataConnection.on('data', data => console.log(`peer ${this.id} received ${data}`)),
-        )
     }
 
+    /**
+     * Initializes the PeerJs {@link Peer} and resolves when a connection to the PeerServer is
+     * established.
+     */
     static async initialize(): Promise<PeerClient> {
         const peerClient = new PeerClient()
 
@@ -17,14 +21,31 @@ class PeerClient extends Peer {
         }))
     }
 
+    /**
+     * Wraps {@link Peer.connect} method for asynchronous use.
+     * Resolves once a connection to the other Peer is established or if the connection fails.
+     * @param peerId The brokering ID of the remote peer (their peer.id).
+     * @param options for specifying details about Peer Connection
+     */
     async asyncConnect(peerId: string, options?: PeerConnectOption): Promise<DataConnection> {
         const dataConnection = this.connect(peerId, options)
+        const timeout = 5000
 
-        return new Promise<DataConnection>(resolve => dataConnection.on('open', () => {
-            console.log(`connection to ${peerId} established`)
+        return new Promise<DataConnection>((resolve, reject) => {
+            setTimeout(reject, timeout)
 
-            resolve(dataConnection)
-        }))
+            dataConnection.on('open', () => {
+                console.log(`connection to ${peerId} established`)
+
+                resolve(dataConnection)
+            })
+
+            dataConnection.on('error', (error) => {
+                console.error(`Connection to ${peerId} failed because of:`, error)
+
+                reject(error)
+            })
+        })
     }
 
     async joinLobby(lobbyId: string) {
@@ -33,10 +54,11 @@ class PeerClient extends Peer {
 }
 
 // Peer Test
-async function testPeer() {
+// TODO write tests for PeerClient
+export async function testPeer() {
     const peer = await PeerClient.initialize()
-
     const peer2 = await PeerClient.initialize()
+
     const dataConnection = await peer.asyncConnect(peer2.id)
     const data = prompt('Message', 'Hi!')
 
