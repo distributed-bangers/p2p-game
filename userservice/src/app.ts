7c83f43cc4907db4930103539ac164deb42a0283
Philipp Erath <philipp.erath@bs-sd.de>
2023-07-26 14:58:41 +0200

    adjusted api-url to userservice

--- userservice/src/app.ts
+++ userservice/src/app.ts
@@ -1,13 +1,14 @@
-import express, { urlencoded } from 'express';
-import userRoutes from './routes/userRoutes.js';
+import express, { urlencoded } from 'express'
+import userRoutes from './routes/userRoutes.js'
+import { apiURL } from './constants/constants.js'
 
 // //* app.js is mainly for applying middleware
 // if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(urlencoded({ extended: true }));
-app.use('/user', userRoutes);
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+app.use(`${apiURL}/games`, userRoutes)
 
-export default app;
+export default app
