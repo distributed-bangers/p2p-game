402c80e14b44b07dbf1b01e83e3ef398eb3e21ee
Philipp Erath <philipp.erath@bs-sd.de>
2023-08-01 17:52:11 +0200

    added basic auth interface (no logic)

---
+++ frontend/src/lib/Auth.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { afterUpdate } from "svelte";
+
+  const signIn = "sign in";
+  const signUp = "sign up";
+  $: signInUC = signIn.toUpperCase();
+  $: signUpUC = signUp.toUpperCase();
+
+  let login = true;
+</script>
+
+<div class="main">
+  <h1>Welcome to Racoosh!</h1>
+
+  <div class="logincard">
+    <h3>Please {login ? signIn : signUp}.</h3>
+    <h3>
+      Want to <a
+        href="#"
+        class="atext"
+        on:click={() => {
+          login = !login;
+        }}
+      >
+        {login ? signUp : signIn}</a
+      > instead?
+    </h3>
+    <input class="input" type="text" placeholder="Username" />
+    <input class="input" type="password" placeholder="Password" />
+    {#if !login}
+      <input class="input" type="password" placeholder="Repeat password" />
+    {/if}
+    <button class="login">{login ? signInUC : signUpUC}</button>
+  </div>
+</div>
+
+<style>
+  .logincard {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: darkgrey;
+    margin-top: 5em;
+    padding: 2em;
+    box-shadow:
+      rgba(6, 24, 44, 0.4) 0px 0px 0px 2px,
+      rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
+      rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+
+    /* place-items: center; */
+  }
+
+  .input {
+    margin: 1em;
+    height: 3em;
+    width: 50%;
+  }
+
+  .atext {
+    color: darkblue;
+    text-decoration: underline;
+  }
+</style>
