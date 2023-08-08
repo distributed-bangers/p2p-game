84f77f45c16da6dc73008d7dd027eb01c04719d9
Philipp Erath <philipp.erath@bs-sd.de>
2023-08-08 10:48:32 +0200

    Added userid to response from userservice/createuser

--- userservice/src/controllers/userController.ts
+++ userservice/src/controllers/userController.ts
@@ -1,62 +1,61 @@
 import {
-  createUserAsync,
-  getOneUserAsync,
-  loginUserAsync,
-} from '../services/userServices.js';
-import { Request, Response } from 'express';
-import { IUser, TypedRequestQuery } from '../index.js';
+    createUserAsync,
+    getOneUserAsync,
+    loginUserAsync,
+} from '../services/userServices.js'
+import { Request, Response } from 'express'
+import { IUser, TypedRequestQuery } from '../index.js'
 
 export const signIn = async (req: Request, res: Response) => {
-  try {
-    const token = await createUserAsync(req.body);
-    res.status(201).json({
-      status: 'success',
-      token: token,
-    });
-  } catch (error: any) {
-    res.status(400).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
-};
+    try {
+        const response = await createUserAsync(req.body)
+        res.status(201).json({
+            userid: response.userid,
+            status: 'success',
+            token: response.token,
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        })
+    }
+}
 
 export const login = async (req: Request, res: Response) => {
-  try {
-    const token = await loginUserAsync(req.body);
-    res.status(201).json({
-      status: 'success',
-      token: token,
-    });
-  } catch (error: any) {
-    res.status(400).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
-};
+    try {
+        const token = await loginUserAsync(req.body)
+        res.status(201).json({
+            status: 'success',
+            token: token,
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        })
+    }
+}
 
 export const getOneUser = async (
-  req: TypedRequestQuery<{ id: string }>,
-  res: Response
+    req: TypedRequestQuery<{ id: string }>,
+    res: Response
 ): Promise<void> => {
-  try {
-    console.log(req.query.id);
-    const user: IUser = <IUser>await getOneUserAsync(req.query.id);
-    res.status(201).json({
-      status: 'success',
-        user:
-    {
-      id: user._id,
-      username: user.username,
-      createdDate: user.createdDate
+    try {
+        console.log(req.query.id)
+        const user: IUser = <IUser>await getOneUserAsync(req.query.id)
+        res.status(201).json({
+            status: 'success',
+            user: {
+                id: user._id,
+                username: user.username,
+                createdDate: user.createdDate,
+            },
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        })
     }
-
-    });
-  } catch (error: any) {
-    res.status(400).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
-};
+}
--- userservice/src/services/userServices.ts
+++ userservice/src/services/userServices.ts
@@ -1,78 +1,80 @@
-import User from '../model/userModel.js';
-import { createHash, generateJWT } from '../auth.js';
-import { IUser } from '../index.js';
-import crypto from "crypto";
+import User from '../model/userModel.js'
+import { createHash, generateJWT } from '../auth.js'
+import { IUser } from '../index.js'
+import crypto from 'crypto'
 
 export async function loginUserAsync(body: any) {
-  let { username, password } = body;
-  if (!username || !password) {
-    throw new Error('Bad request');
-  }
-  const user = await checkUserExistence(username)
-  if (!user) {
-    throw new Error("User doesn't exists!");
-  } else if (user.password == createHash(user.salt,password)) {
-    try {
-      const token = generateJWT(user);
-      return token!;
-    } catch (error) {
-      throw error;
+    let { username, password } = body
+    if (!username || !password) {
+        throw new Error('Bad request')
     }
-  } else {
-    throw new Error("Password doesn't match");
-  }
+    const user = await checkUserExistence(username)
+    if (!user) {
+        throw new Error("User doesn't exists!")
+    } else if (user.password == createHash(user.salt, password)) {
+        try {
+            const token = generateJWT(user)
+            return token!
+        } catch (error) {
+            throw error
+        }
+    } else {
+        throw new Error("Password doesn't match")
+    }
 }
 
-export async function createUserAsync(body: any): Promise<string> {
-  let { username, password } = body;
-  console.log(body)
-  if (!username || !password) {
-    throw new Error('Bad request');
-  }
-  const userExistence = await checkUserExistence(username)
-  if (userExistence) {
-    throw new Error("User with this username already exists}");
-  }
-  else{
-    try {
-      let salt = crypto.randomUUID()
-      const user = new User({
-        username: username,
-        salt: salt,
-        password: createHash(salt,password),
-        createdDate: new Date(),
-      });
-      await user.save();
-      const token = generateJWT(user);
-      return token!;
-    } catch (error) {
-      throw error;
+export async function createUserAsync(
+    body: any
+): Promise<{ userid: string; token: string }> {
+    let { username, password } = body
+    console.log(body)
+    if (!username || !password) {
+        throw new Error('Bad request')
     }
-  }
+    const userExistence = await checkUserExistence(username)
+    if (userExistence) {
+        throw new Error('User with this username already exists}')
+    } else {
+        try {
+            let salt = crypto.randomUUID()
+            const user = new User({
+                username: username,
+                salt: salt,
+                password: createHash(salt, password),
+                createdDate: new Date(),
+            })
+            const createdUser = await user.save()
+            const userid = createdUser._id.toString()
+            const token = generateJWT(user)
+            return { userid: userid, token: token }
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
 export async function getOneUserAsync(username: string): Promise<object> {
-  if (!username) {
-    throw new Error('Bad request');
-  }
-  const user = await checkUserExistence(username)
-  if (!user) {
-    throw new Error("User doesn't exists!");
-  } else {
-    return user;
-  }
+    if (!username) {
+        throw new Error('Bad request')
+    }
+    const user = await checkUserExistence(username)
+    if (!user) {
+        throw new Error("User doesn't exists!")
+    } else {
+        return user
+    }
 }
 
-async function checkUserExistence(username: string):Promise<null|IUser>{
-  let existingUser;
-  try {
-    existingUser = <IUser>await User.findOne({ username: username });
-  } catch (error) {
-    throw error;
-  }
-  if (!existingUser) {
-    return null;
-  } else {
-    return existingUser;
-  }
+async function checkUserExistence(username: string): Promise<null | IUser> {
+    let existingUser
+    try {
+        existingUser = <IUser>await User.findOne({ username: username })
+    } catch (error) {
+        throw error
+    }
+    if (!existingUser) {
+        return null
+    } else {
+        return existingUser
+    }
 }
