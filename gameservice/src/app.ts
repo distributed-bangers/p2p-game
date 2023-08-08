de6eaefaf622dc9d9c827f4cb7b62cc9d28c1ed1
Philipp Erath <philipp.erath@bs-sd.de>
2023-08-08 10:21:27 +0200

    Added CORS-policy to backend

--- gameservice/src/app.ts
+++ gameservice/src/app.ts
@@ -2,12 +2,21 @@ import express, { urlencoded } from 'express'
 import gameRoutes from './routes/gameRoutes.js'
 import { apiURL } from './constants/constants.js'
 import { createServer } from 'http'
+import cors, { CorsOptions } from 'cors'
 
 // //* app.js is mainly for applying middleware
 // if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+const clientUrl = process.env.CLIENT_URL
+
+let corsOptions: CorsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+}
+
 const app = express()
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(urlencoded({ extended: true }))
 app.use(`${apiURL}/games`, gameRoutes)
--- userservice/src/app.ts
+++ userservice/src/app.ts
@@ -1,12 +1,22 @@
 import userRoutes from './routes/userRoutes.js'
 import { apiURL } from './constants/constants.js'
 import express, { urlencoded, json } from 'express'
+import cors, { CorsOptions } from 'cors'
+import config from 'config'
 
 // //* app.js is mainly for applying middleware
 // if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+const clientUrl = <string>config.get('client.url')
+
+let corsOptions: CorsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+}
+
 const app = express()
 
+app.use(cors(corsOptions))
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(`${apiURL}/users/`, userRoutes)
