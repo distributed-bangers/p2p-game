6102703b9c00e089a259efc9c2cdacc4d3421506
Philipp Erath <philipp.erath@bs-sd.de>
2023-08-10 17:30:06 +0200

    added async to leaving rooms in socketio-server

--- gameservice/src/socketios.ts
+++ gameservice/src/socketios.ts
@@ -21,16 +21,16 @@ io.on('connection', (socket) => {
 
     //* both join and leave are send by the players (not the host) after calling join/leave endpoint
     //* client attaches sends his own information after joining/leaving (data)
-    socket.on(socketio.join, (data: IUser) => {
+    socket.on(socketio.join, async (data: IUser) => {
         console.log('SOCKETIO-JOIN', data)
-        socket.join(gameId)
+        await socket.join(gameId)
         socket.to(gameId).emit(socketio.join, data)
     })
 
-    socket.on(socketio.leave, (data: IUser) => {
+    socket.on(socketio.leave, async (data: IUser) => {
         console.log('SOCKETIO-LEAVE', data)
         socket.to(gameId).emit(socketio.leave, data)
-        socket.leave(gameId)
+        await socket.leave(gameId)
         socket.disconnect()
     })
 
@@ -40,15 +40,14 @@ io.on('connection', (socket) => {
         socket.to(gameId).emit(socketio.startGame, data)
     })
 
-    //* who can send this?
-    //* disconnects all members of a group
+    //* only host sends this: disconnects all members of a group
     //* should get called if game gets cancelled before starting, breaks during playing or ends successfully
     socket.on(socketio.endConnection, () => {
         socket.to(gameId).emit(socketio.endConnection)
         const roomClients = io.sockets.adapter.rooms.get(gameId)
-        roomClients?.forEach((client) => {
+        roomClients?.forEach(async (client) => {
             const clientSocket = io.sockets.sockets.get(client)
-            clientSocket?.leave(gameId)
+            await clientSocket?.leave(gameId)
             clientSocket?.disconnect()
         })
     })
