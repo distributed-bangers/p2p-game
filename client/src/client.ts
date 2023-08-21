07324bd08ce43261d7f50c2b98853e57169c3ac0
Philipp Erath <philipp.erath@bs-sd.de>
2023-08-21 18:12:01 +0200

    last corrections to client usage in frontend (still wip)

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,163 +1,166 @@
-import PeerClient from './peerClient'
-import { DataConnection } from 'peerjs'
-import Renderer from './renderer'
-import { Bullet, Player } from './game/objects'
-import { isSceneSnapshot, SceneSnapshot } from './game/snapshots'
-
+import PeerClient from './peerClient';
+import { DataConnection } from 'peerjs';
+import Renderer from './renderer';
+import { Bullet, Player } from './game/objects';
+import { isSceneSnapshot, SceneSnapshot } from './game/snapshots';
 
 interface GameState {
-    [playerId: string]: Player
+  [playerId: string]: Player;
 }
 
 interface Peer {
-    connection: DataConnection
-    id: string
+  connection: DataConnection;
+  id: string;
 }
 
 export interface Inputs {
-    moveDown: boolean
-    moveLeft: boolean
-    moveRight: boolean
-    moveUp: boolean
-    shoot: boolean
+  moveDown: boolean;
+  moveLeft: boolean;
+  moveRight: boolean;
+  moveUp: boolean;
+  shoot: boolean;
 }
 
 export class GameClient {
-    private readonly id: string
+  private readonly id: string;
 
-    private peerClient: PeerClient
-    private peers: Peer[] = []
+  private peerClient: PeerClient;
+  private peers: Peer[] = [];
 
-    renderer: Renderer = new Renderer(this)
+  renderer: Renderer = new Renderer(this);
 
-    gameId: string = ''
-    player: Player = new Player('blue')
-    state: GameState = {}
+  gameId: string = '';
+  player: Player = new Player('blue');
+  state: GameState = {};
 
-    private constructor(peerClient: PeerClient) {
-        this.peerClient = peerClient
-        this.peerClient.on('connection', (dataConnection) => {
-            this.onConnection(dataConnection)
-        })
+  private constructor(peerClient: PeerClient) {
+    this.peerClient = peerClient;
+    this.peerClient.on('connection', (dataConnection) => {
+      this.onConnection(dataConnection);
+    });
 
-        this.id = peerClient.id
-        this.state[this.id] = this.player
-        this.renderer.addPlayer(this.player)
+    this.id = peerClient.id;
+    this.state[this.id] = this.player;
+    this.renderer.addPlayer(this.player);
 
-        document.addEventListener('keydown', ev => this.onKeyDown(ev, this))
-        document.addEventListener('keyup', ev => this.onKeyUp(ev, this))
+    document.addEventListener('keydown', (ev) => this.onKeyDown(ev, this));
+    document.addEventListener('keyup', (ev) => this.onKeyUp(ev, this));
+  }
+
+  async startGame(gameId: string, users: string[]) {
+    this.gameId = gameId;
+
+    const promises = [];
+
+    for (let index = 0; index < users.length; index++) {
+      promises.push(this.connect(users[index]).then());
     }
 
-    async startGame(gameId: string, users: string[]) {
-        this.gameId = gameId
+    return Promise.all(promises);
+  }
 
-        return Promise.all(users.map(this.connect))
-    }
+  onGameFinished(callBack: (highscore: number) => any) {}
 
-    onGameFinished(callBack: (highscore: number) => any) {
+  static async initialize(id?: string) {
+    const peerClient = await PeerClient.initialize(id);
 
-    }
+    return new GameClient(peerClient);
+  }
 
-    static async initialize(id?: string) {
-        const peerClient = await PeerClient.initialize(id)
+  private onConnection(dataConnection: DataConnection) {
+    const peer: Peer = { id: dataConnection.peer, connection: dataConnection };
+    this.peers.push(peer);
 
-        return new GameClient(peerClient)
-    }
+    const player = new Player('red');
+    this.state[peer.id] = player;
+    this.renderer.addPlayer(player);
 
-    private onConnection(dataConnection: DataConnection) {
-        const peer: Peer = { id: dataConnection.peer, connection: dataConnection }
-        this.peers.push(peer)
+    dataConnection.on('data', (data) => {
+      if (isSceneSnapshot(data)) {
+        const player = this.state[dataConnection.peer];
+        player.setFromSnapshot(data.player);
 
-        const player = new Player('red')
-        this.state[peer.id] = player
-        this.renderer.addPlayer(player)
-
-        dataConnection.on('data', (data) => {
-            if (isSceneSnapshot(data)) {
-                const player = this.state[dataConnection.peer]
-                player.setFromSnapshot(data.player)
-
-                while (player.bullets.length !== data.bullets.length) {
-                    if (player.bullets.length < data.bullets.length) {
-                        player.spawnBullet()
-                    } else {
-                        this.renderer.scene.remove(player.bullets.pop() as Bullet)
-                    }
-                }
-
-                player.bullets.forEach((bullet, index) => bullet.setFromSnapshot(data.bullets[index]))
-            }
-        })
-
-    }
-
-    private onKeyDown(event: KeyboardEvent, client: GameClient) {
-        const inputs = client.player.inputs
-
-        switch (event.code) {
-            case 'ArrowUp':
-            case 'KeyW':
-                inputs.moveUp = true
-                break
-            case 'ArrowLeft':
-            case 'KeyA':
-                inputs.moveLeft = true
-                break
-            case 'ArrowDown':
-            case 'KeyS':
-                inputs.moveDown = true
-                break
-            case 'ArrowRight':
-            case 'KeyD':
-                inputs.moveRight = true
-                break
-            case 'Space':
-                inputs.shoot = true
-                break
-        }
-    }
-
-    private onKeyUp(event: KeyboardEvent, client: GameClient) {
-        const inputs = client.player.inputs
-
-        switch (event.code) {
-            case 'ArrowUp':
-            case 'KeyW':
-                inputs.moveUp = false
-                break
-            case 'ArrowLeft':
-            case 'KeyA':
-                inputs.moveLeft = false
-                break
-            case 'ArrowDown':
-            case 'KeyS':
-                inputs.moveDown = false
-                break
-            case 'ArrowRight':
-            case 'KeyD':
-                inputs.moveRight = false
-                break
-            case 'Space':
-                inputs.shoot = false
-                break
-        }
-    }
-
-    async connect(id: string) {
-        const dataConnection = await this.peerClient.uniqueConnect(id)
-
-        this.onConnection(dataConnection)
-    }
-
-    sendSnapshot() {
-        const snapshot: SceneSnapshot = {
-            bullets: this.player.bullets.map(bullet => bullet.getSnapshot()),
-            player: this.player.getSnapshot(),
+        while (player.bullets.length !== data.bullets.length) {
+          if (player.bullets.length < data.bullets.length) {
+            player.spawnBullet();
+          } else {
+            this.renderer.scene.remove(player.bullets.pop() as Bullet);
+          }
         }
 
-        for (const peer of this.peers) {
-            peer.connection.send(snapshot)
-        }
+        player.bullets.forEach((bullet, index) =>
+          bullet.setFromSnapshot(data.bullets[index])
+        );
+      }
+    });
+  }
+
+  private onKeyDown(event: KeyboardEvent, client: GameClient) {
+    const inputs = client.player.inputs;
+
+    switch (event.code) {
+      case 'ArrowUp':
+      case 'KeyW':
+        inputs.moveUp = true;
+        break;
+      case 'ArrowLeft':
+      case 'KeyA':
+        inputs.moveLeft = true;
+        break;
+      case 'ArrowDown':
+      case 'KeyS':
+        inputs.moveDown = true;
+        break;
+      case 'ArrowRight':
+      case 'KeyD':
+        inputs.moveRight = true;
+        break;
+      case 'Space':
+        inputs.shoot = true;
+        break;
     }
+  }
+
+  private onKeyUp(event: KeyboardEvent, client: GameClient) {
+    const inputs = client.player.inputs;
+
+    switch (event.code) {
+      case 'ArrowUp':
+      case 'KeyW':
+        inputs.moveUp = false;
+        break;
+      case 'ArrowLeft':
+      case 'KeyA':
+        inputs.moveLeft = false;
+        break;
+      case 'ArrowDown':
+      case 'KeyS':
+        inputs.moveDown = false;
+        break;
+      case 'ArrowRight':
+      case 'KeyD':
+        inputs.moveRight = false;
+        break;
+      case 'Space':
+        inputs.shoot = false;
+        break;
+    }
+  }
+
+  async connect(id: string) {
+    const dataConnection = await this.peerClient.uniqueConnect(id);
+
+    this.onConnection(dataConnection);
+  }
+
+  sendSnapshot() {
+    const snapshot: SceneSnapshot = {
+      bullets: this.player.bullets.map((bullet) => bullet.getSnapshot()),
+      player: this.player.getSnapshot(),
+    };
+
+    for (const peer of this.peers) {
+      peer.connection.send(snapshot);
+    }
+  }
 }
-
--- frontend/src/lib/Game.svelte
+++ frontend/src/lib/Game.svelte
@@ -1,6 +1,7 @@
-<script>
+<script lang="ts">
+  
   import { onMount } from 'svelte';
-  import {gameClient} from '../../../client/src/client';
+  import {GameClient} from '../../../client/src/client';
   import userState from '../../state/user';
     import { get } from 'svelte/store';
 
@@ -8,14 +9,14 @@
     let playerIds = [];
     get(userState).game.players.map(p => playerIds.push(p.userid));
     console.log('PLAYERS RECEIVED', playerIds);
-    gameClient.startGame($userState.game._id, playerIds);
+    const canvas  = document.getElementById('canvas') as HTMLCanvasElement;
+    const clientId =get(userState).userid;
+    const otherPlayerIds = playerIds.filter(p => p !== clientId);
+    const gameClient = await GameClient.initialize(clientId);
+    await gameClient.startGame($userState.game._id, otherPlayerIds);
+    console.log('after start game');
+    gameClient.renderer.initialize(canvas, canvas.clientWidth, canvas.clientHeight);
   });
-
-
-  const onGameFinishes = async () => {
-
-
-  };
 
 </script>
 
