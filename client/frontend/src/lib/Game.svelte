4a8ab21feb166a8f97746a4cd0701711ee429236
Sandesh Dulal <dulals.tin21@student.dhbw-heidenheim.de>
2023-08-23 16:28:51 +0200

    Project structor changed

---
+++ client/frontend/src/lib/Game.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  
+  import { onMount } from 'svelte';
+  import {GameClient} from '../../../game/src/client';
+  import userState from '../../state/user';
+    import { get } from 'svelte/store';
+
+  onMount(async () => {
+    let playerIds = [];
+    get(userState).game.players.map(p => playerIds.push(p.userid));
+    console.log('PLAYERS RECEIVED', playerIds);
+    const canvas  = document.getElementById('canvas') as HTMLCanvasElement;
+    const clientId =get(userState).userid;
+    const otherPlayerIds = playerIds.filter(p => p !== clientId);
+    const gameClient = await GameClient.initialize(clientId);
+    await gameClient.startGame($userState.game._id, otherPlayerIds);
+    console.log('AFTER START GAME', otherPlayerIds.length);
+    gameClient.renderer.initialize(canvas, canvas.clientWidth, canvas.clientHeight);
+  });
+
+</script>
+
+  <canvas id="canvas"></canvas>
+
+<style>
+#canvas {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
