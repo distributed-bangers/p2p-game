823bd3fe3af9ed4522191c1c65703f42fda676cc
Lasse Herzog <herzogl.tin21@student.dhbw-heidenheim.de>
2023-09-27 17:59:32 +0200

    Added Prettier and Eslint to game client

--- client/game/src/test.ts
+++ client/game/src/test.ts
@@ -1,12 +1,12 @@
-import { GameClient } from './client'
+import { GameClient } from "./client";
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
-const gameClient = await GameClient.initialize()
+const gameClient = await GameClient.initialize();
 
-gameClient.renderer.initialize(canvas, window.innerWidth, window.innerHeight)
+gameClient.renderer.initialize(canvas, window.innerWidth, window.innerHeight);
 
-const otherId: string | null = prompt('Other peer id')
-const users = otherId === null ? [] : [otherId]
+const otherId: string | null = prompt("Other peer id");
+const users = otherId === null ? [] : [otherId];
 
-await gameClient.startGame('test', users)
+await gameClient.startGame("test", users);
