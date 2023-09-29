559ab79012eb621f7c21e6718d193d5adae34ea0
dulals.tin21 <dulals.tin21@student.dhbw-heidenheim.de>
2023-09-29 20:37:46 +0200

    Two users added

--- mongo-init-users.js
+++ mongo-init-users.js
@@ -2,12 +2,24 @@ db = new Mongo().getDB("userservice");
 
 db.createCollection("users", { capped: false });
 
-// db.users.insert([
-//   { username: 'sandesh', password: '1234', date: '2000-01-01' },
-//   { username: 'sandesh', password: '1234', date: '2000-01-01' },
-//   { username: 'sandesh', password: '1234', date: '2000-01-01' },
-//   { username: 'sandesh', password: '1234', date: '2000-01-01' },
-// ]);
+db.users.insert([
+  {
+    _id: '6516c437de09212b862b6165',
+    username: 'User1',
+    salt: '98b48859-8e2b-46fa-8fc7-f800c77b50bb',
+    password: '2137db5d61733d18511a43cd5ff454f13d1e6000c6096a413c0f2b9e945a562c',
+    createdDate: '2023-09-29T12:33:59.857Z'
+  },
+  {
+    _id: '6516c437de09212b862b6166',
+    username: 'User2',
+    salt: '98b48859-8e2b-46fa-8fc7-f800c77b50bb',
+    password: '2137db5d61733d18511a43cd5ff454f13d1e6000c6096a413c0f2b9e945a562c',
+    createdDate: '2023-09-29T12:33:59.857Z'
+  },
+]);
+// username: User1, password: User1!User1!
+// username: User2, password: User1!User1!
 
 db.createUser({
   user: "us_admin",
